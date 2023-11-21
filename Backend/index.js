70492e50d2aad513a7f2630d23334bef7603ef35
Dileep <dileepvunnam@gmail.com>
2023-11-21 02:12:17 +0000

    User routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,37 @@
 import express from 'express';
+import { config } from 'dotenv';
+import mongoose from 'mongoose';
+import userRoutes from './routes/userRoutes';
+
+config({ path: `.env.${process.env.NODE_ENV}` });
+
 const app = express();
 
+// app.use(cors({
+//     origin: 'https://chitter-zbe7.onrender.com',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
+//     credentials: true
+// }));
+app.use(express.json());
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log('MongoDB Connection Error:', err));
+
+
+app.use('/api/users', userRoutes);
+app.use((err, req, res, next) => {
+    res.status(400).json({ msg: err.message });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
