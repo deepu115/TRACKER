b11eb9bc5908073fa43d28f3a8220553e0e9520e
Dileep <dileepvunnam@gmail.com>
2023-11-21 02:17:53 +0000

    auth middleware

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+export const protect = (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Get token from header
+            token = req.headers.authorization.split(' ')[1];
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+            // Get user from the token
+            req.user = decoded;
+            next();
+        } catch (error) {
+            res.status(401).json({ message: 'Not authorized, token failed' });
+        }
+    }
+
+    if (!token) {
+        res.status(401).json({ message: 'Not authorized, no token' });
+    }
+};
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import { signup, login, refreshAccessToken } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/refresh', refreshAccessToken);
+router.get('/profile', protect, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.userId).select('-password'); // Exclude password from the result
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+});
 
 export default router;
