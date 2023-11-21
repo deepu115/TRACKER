2f5ca3a9c97a1dba6e54b37120aa6496bf9ee971
Dileep <dileepvunnam@gmail.com>
2023-11-21 01:55:13 +0000

    encryption algorithm

---
+++ Backend/services/encryptionService.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const saltRounds = 10;
+const accessTokenExpiry = '15m'; // Short-lived access token
+const refreshTokenExpiry = '7d'; // Long-lived refresh token
+
+export const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(saltRounds);
+    return bcrypt.hash(password, salt);
+};
+
+export const comparePassword = async (password, hash) => {
+    return bcrypt.compare(password, hash);
+};
+
+export const createAccessToken = (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_ACCESS_SECRET, { expiresIn: accessTokenExpiry });
+};
+
+export const createRefreshToken = (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, { expiresIn: refreshTokenExpiry });
+};
