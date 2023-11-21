768cca36656daa0fc87ad75003836a62fd5fc0db
Dileep <dileepvunnam@gmail.com>
2023-11-21 23:52:47 +0000

    updated user routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,24 +2,14 @@ import express from 'express';
 import { signup, login, refreshAccessToken } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { userSignupValidationRules, userLoginValidationRules, validate } from '../middleware/userValidation.js';
+import { getUserProfile, updateUserProfile } from '../controllers/userProfileController.js';
 import User from '../models/user.js';
 const router = express.Router();
 
 router.post('/signup', userSignupValidationRules(), validate, signup);
 router.post('/login', userLoginValidationRules(), validate, login);
 router.post('/refresh', refreshAccessToken);
-router.get('/profile', protect, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.userId).select('-password'); // Exclude password from the result
-        if (user) {
-            res.json(user);
-        } else {
-            res.status(404).send('User not found');
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(500).send('Server error');
-    }
-});
+router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 
 export default router;
